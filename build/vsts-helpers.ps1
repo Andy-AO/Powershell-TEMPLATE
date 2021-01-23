30b6225abf4e50d1e5be0d4a525d2958e71b4da5
zen96285 <zen96285@gmail.com>
2021-01-23 13:54:42 +0800

    .refactor : Remove two duplicate string concatenation operations.

--- build/vsts-helpers.ps1
+++ build/vsts-helpers.ps1
@@ -1,5 +1,4 @@
-function WriteHeader
-{
+function WriteHeader {
 	param
 	(
 		[Parameter(Position = 0, Mandatory = $true)]
@@ -18,10 +17,8 @@ function WriteHeader
 	# Add some extra spacing to either side of the message.
 	$length = $Message.Length + 2
 	
-	function WriteBorder($remark){
-		$HelpMessage += "+"
-		$HelpMessage += ("-" * $length)
-		$HelpMessage += "+$([Environment]::NewLine)" 
+	function WriteBorder($remark) {
+		$HelpMessage += "+$("-" * $length)+$([Environment]::NewLine)" 
 	}
 
 	. WriteBorder 'Top'
